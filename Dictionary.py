741d866e8b0bb55ef2dcfd2bcee1daa41dfcb946
Divesh <166918283+sonadivu29@users.noreply.github.com>
2025-03-19 05:49:48 +0000

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+print("Hello Everyone, Let's Study Dictionary")
+
+Dictionary = {"Name":"Divesh", "Age":20, "College":"Keystone", "Education":"B.E(Computer)", "Address":"Akurdi", "Contact":9359978157}
+
+print("Display all items of the dictionary:", Dictionary)
+
+print("Display only keys of Dictionary:", Dictionary.keys)
+print("Display only values of Dictionary:", Dictionary.values)
+
+print("Slicing on Dictionary:", Dictionary[2:4])
+
