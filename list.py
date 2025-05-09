00304f5a0eedb4d16c19f9a2d21dc008b37f765f
Divesh <166918283+sonadivu29@users.noreply.github.com>
2025-05-09 09:55:15 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+print("List Explanation")
+list = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+print("The list is:",list)
+
+print("Access elements with index:\n")
+print("Display 5th index element from list:", list.index(5))
