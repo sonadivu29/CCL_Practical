c6a1ee55a8e58e94d1cef30097e872de56c0eaef
Divesh <166918283+sonadivu29@users.noreply.github.com>
2025-05-09 10:04:19 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,5 +3,5 @@ list = [11, 12, 13, 14, 15, 16, 17, 18, 19]
 
 print("The list is:",list)
 
-print("Access elements with index:\n")
-print("Display 5th index element from list:", list.index(5))
+print("index method of list:\n")
+print("Display index of 16 from list:", list.index(16))
